2c7ede70a2e0f5fef775943f96b960d50fa902d9
Karol Chmist <karolchmist@gmail.com>
2016-01-27 19:04:06 +0100

    Clean up in OptionalTest

--- src/test/scala/OptionalTest.scala
+++ src/test/scala/OptionalTest.scala
@@ -1,4 +1,4 @@
-import monocle.{Lens, PLens}
+import monocle.{Optional, POptional, Lens, PLens}
 import org.specs2.mutable.Specification
 
 
@@ -11,7 +11,7 @@ class OptionalTest extends Specification {
 
     import monocle.macros.Lenses
 
-    @Lenses("_") case class Street(name: String, price: Double = 0)
+    @Lenses("_") case class Street(name: String)
     @Lenses("_") case class Address(street: Street)
     @Lenses("_") case class Company(address: Address)
     @Lenses("_") case class Employee(company: Company)
@@ -20,21 +20,25 @@ class OptionalTest extends Specification {
       import monocle.function.all.headOption
       import monocle.std.string._
 
-      val address = Address(Street("rue lavoisier"))
-      val newAddress: Address = (Address._street composeLens Street._name composeOptional headOption).modify(_.toUpper)(address)
-      newAddress ==== Address(Street("Rue lavoisier"))
+      val address = Address(Street("rue Servient"))
+      val optionlAddressToFirstLetter: Optional[Address, Char] = Address._street composeLens Street._name composeOptional headOption
+      val newAddress: Address = optionlAddressToFirstLetter.modify(_.toUpper)(address)
+      newAddress ==== Address(Street("Rue Servient"))
     }
-    "index" in {
+
+    "index + modify" in {
       import monocle.function.all.index
       import monocle.std.list.listIndex
 
-      @Lenses
-      case class Carte(streets: List[Street])
+      @Lenses("_") case class Carte(streets: List[Street])
 
-      val carte = Carte(List(Street("rue abc")))
+      val carte = Carte(List(Street("rue Servient"), Street("cours gambetta")))
 
-      val newCarte: Carte = (Carte.streets composeOptional index(0) composeLens Street._name).modify(_.toUpperCase)(carte)
-      newCarte ==== Carte(List(Street("RUE ABC")))
+      val optionalCarteToFirstStreetName: Optional[Carte, String] = Carte._streets composeOptional index(1) composeLens Street._name
+
+      val modifiedCarte: Carte = optionalCarteToFirstStreetName.modify(_.toUpperCase)(carte)
+
+      modifiedCarte ==== Carte(List(Street("rue Servient"), Street("COURS GAMBETTA")))
     }
 
   }
