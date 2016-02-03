c9ff97144065c05055eecc730121e09903255c67
Karol Chmist <karolchmist@gmail.com>
2016-02-03 22:28:33 +0100

    Add example with lense of tuples and each composition in PrismTest.

--- src/test/scala/PrismTest.scala
+++ src/test/scala/PrismTest.scala
@@ -21,5 +21,29 @@ class PrismTest extends Specification {
       Character._hero.getOption(hero) ==== Some(Hero(level = 12))
       Character._hero.getOption(enemy) ==== None
     }
+
+    "lense tuple" in {
+      val hero: Hero = Hero(level = 12, name = "John")
+
+      val _heroLevelName = Lens[Hero, (Int, String)](h => (h.level, h.name))
+        {case (l,n) => h => h.copy(level = l, name = n)}
+
+      _heroLevelName.modify{case (l,n) => (l+1, n.toUpperCase)}(hero) ==== Hero(13,"JOHN")
+    }
+
+    "each + optional composition" in {
+      import monocle.function.Each.each
+      import monocle.std.list.listEach
+
+      val _heroLevel = GenLens[Hero](h => h.level)
+      val _characterLevel = Character._hero composeLens _heroLevel
+      val _gainLevel = each[List[Character], Character] composeOptional _characterLevel
+
+      val characters: List[Character] = List(Hero(level = 12, name = "John"), Enemy(name = "Dragon"))
+
+      val betterHeroes = _gainLevel.modify(_ + 1)(characters)
+
+      betterHeroes  ==== List(Hero(level = 13, name = "John"), Enemy(name = "Dragon"))
+    }
   }
 }
